407ccbe1b52b6e159c251b9adff611942ec6af96
chris.belakhoua <chris.belakhoua@ert.com>
2020-06-23 18:22:48 -0500

    Sticky header menu work

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -2,8 +2,10 @@ import "../styles/styles.css";
 import MenuItem from "./modules/MobileMenu";
 import MobileMenu from "./modules/MobileMenu";
 import RevealOnScroll from "./modules/RevealOnScroll";
+import StickyHeader from "./modules/StickyHeader";
 
 const mobileMenu = new MobileMenu();
+const stickyHeader = new StickyHeader();
 new RevealOnScroll(document.querySelectorAll(".feature-item"), 60);
 new RevealOnScroll(document.querySelectorAll(".testimonial"), 75);
 
